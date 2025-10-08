fe0574733d9a7edbda1246496659f484c0e37a46
rostislavpas <rostislav.pas@gmail.com>
2025-10-08 20:28:59 +0300

    fix logic wayforpay and keycrm v6

--- payments/views.py
+++ payments/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+from datetime import timedelta
 import hmac
 import time
 import hashlib
