b4fc5169e29bbe0201784af9d489761041276ad9
rostislavpas <rostislav.pas@gmail.com>
2025-11-29 16:37:21 +0200

    commented opening page landing

--- payments/views.py
+++ payments/views.py
@@ -38,14 +38,14 @@ def mobile(request):
     return render(request, 'mobile.html')
 
 
-def opening(request):
-    """Сторінка Grand Opening Party (колишня index)"""
-    return render(request, "opening.html")
+# def opening(request):
+#     """Сторінка Grand Opening Party (колишня index)"""
+#     return render(request, "opening.html")
 
 
-def opening_mobile(request):
-    """Мобільна сторінка Grand Opening Party (колишня mobile)"""
-    return render(request, "opening_mobile.html")
+# def opening_mobile(request):
+#     """Мобільна сторінка Grand Opening Party (колишня mobile)"""
+#     return render(request, "opening_mobile.html")
 
 
 def subscription(request):
